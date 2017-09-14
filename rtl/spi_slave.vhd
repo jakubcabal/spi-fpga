9cba5002a124accaa9b4eeb4df79b6c0473a2f64
Jakub Cabal <jakubcabal@gmail.com>
2017-09-14 19:40:14 +0200

    SPI_SLAVE: Added comments, some small edits.

--- rtl/spi_slave.vhd
+++ rtl/spi_slave.vhd
@@ -41,9 +41,9 @@ entity SPI_SLAVE is
         MOSI     : in  std_logic;
         MISO     : out std_logic;
         -- USER INTERFACE
-        READY    : out std_logic; -- when READY = 1, SPI slave is ready to accept input data
+        READY    : out std_logic; -- when READY = 1, valid input data are accept
         DIN      : in  std_logic_vector(7 downto 0); -- input data for master
-        DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, input data are valid and can be accept
+        DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, input data are valid
         DOUT     : out std_logic_vector(7 downto 0); -- output data from master
         DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, output data are valid
     );
@@ -54,25 +54,22 @@ architecture RTL of SPI_SLAVE is
     signal spi_clk_reg        : std_logic;
     signal spi_clk_redge_en   : std_logic;
     signal spi_clk_fedge_en   : std_logic;
-    signal load_data          : std_logic;
-    signal data_shreg         : std_logic_vector(7 downto 0);
     signal bit_cnt            : unsigned(2 downto 0);
+    signal bit_cnt_max        : std_logic;
     signal last_bit_en        : std_logic;
+    signal load_data_en       : std_logic;
+    signal data_shreg         : std_logic_vector(7 downto 0);
     signal slave_ready        : std_logic;
-    signal data_busy_reg      : std_logic;
+    signal shreg_busy         : std_logic;
     signal rx_data_vld        : std_logic;
 
 begin
-
-    load_data <= slave_ready and DIN_VLD;
-    READY     <= slave_ready;
-    DOUT      <= data_shreg;
-    DOUT_VLD  <= rx_data_vld;
 
     -- -------------------------------------------------------------------------
     --  SPI CLOCK REGISTER
     -- -------------------------------------------------------------------------
 
+    -- The SPI clock register is necessary for clock edge detection.
     spi_clk_reg_p : process (CLK)
     begin
         if (rising_edge(CLK)) then
@@ -88,75 +85,24 @@ begin
     --  SPI CLOCK EDGES FLAGS
     -- -------------------------------------------------------------------------
 
+    -- Falling edge is detect when SCLK=0 and spi_clk_reg=1.
     spi_clk_fedge_en <= not SCLK and spi_clk_reg;
+    -- Rising edge is detect when SCLK=1 and spi_clk_reg=0.
     spi_clk_redge_en <= SCLK and not spi_clk_reg;
 
     -- -------------------------------------------------------------------------
-    --  DATA BUSY REGISTER
+    --  RECEIVED BITS COUNTER
     -- -------------------------------------------------------------------------
 
-    rx_data_vld <= spi_clk_fedge_en and last_bit_en;
-
-    data_busy_reg_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (RST = '1') then
-                data_busy_reg <= '0';
-            else
-                if (DIN_VLD = '1' and (CS_N = '1' or rx_data_vld = '1')) then
-                    data_busy_reg <= '1';
-                elsif (rx_data_vld = '1') then
-                    data_busy_reg <= '0';
-                else
-                    data_busy_reg <= data_busy_reg;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    slave_ready <= (CS_N and not data_busy_reg) or rx_data_vld;
-
-    -- -------------------------------------------------------------------------
-    --  MISO REGISTER
-    -- -------------------------------------------------------------------------
-
-    miso_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (load_data = '1') then
-                MISO <= DIN(7);
-            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
-                MISO <= data_shreg(7);
-            end if;
-        end if;
-    end process;
-
-    -- -------------------------------------------------------------------------
-    --  DATA SHIFT REGISTER
-    -- -------------------------------------------------------------------------
-
-    data_shreg_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (load_data = '1') then
-                data_shreg <= DIN;
-            elsif (spi_clk_redge_en = '1' and CS_N = '0') then
-                data_shreg <= data_shreg(6 downto 0) & MOSI;
-            end if;
-        end if;
-    end process;
-
-    -- -------------------------------------------------------------------------
-    --  BIT COUNTER
-    -- -------------------------------------------------------------------------
-
+    -- The counter counts received bits from the master. Counter is enabled when
+    -- falling edge of SPI clock is detected and not asserted CS_N.
     bit_cnt_p : process (CLK)
     begin
         if (rising_edge(CLK)) then
             if (RST = '1') then
                 bit_cnt <= (others => '0');
             elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
-                if (bit_cnt = "111") then
+                if (bit_cnt_max = '1') then
                     bit_cnt <= (others => '0');
                 else
                     bit_cnt <= bit_cnt + 1;
@@ -165,23 +111,104 @@ begin
         end if;
     end process;
 
+    -- The flag of maximal value of the bit counter.
+    bit_cnt_max <= '1' when (bit_cnt = "111") else '0';
+
     -- -------------------------------------------------------------------------
     --  LAST BIT FLAG REGISTER
     -- -------------------------------------------------------------------------
 
+    -- The flag of last bit of received byte is only registered the flag of
+    -- maximal value of the bit counter.
     last_bit_en_p : process (CLK)
     begin
         if (rising_edge(CLK)) then
             if (RST = '1') then
                 last_bit_en <= '0';
             else
-                if (bit_cnt = "111") then
-                    last_bit_en <= '1';
+                last_bit_en <= bit_cnt_max;
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  RECEIVED DATA VALID FLAG
+    -- -------------------------------------------------------------------------
+
+    -- Received data from master are valid when falling edge of SPI clock is
+    -- detected and the last bit of received byte is detected.
+    rx_data_vld <= spi_clk_fedge_en and last_bit_en;
+
+    -- -------------------------------------------------------------------------
+    --  SHIFT REGISTER BUSY FLAG REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- Data shift register is busy until it sends all input data to SPI master.
+    shreg_busy_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                shreg_busy <= '0';
+            else
+                if (DIN_VLD = '1' and (CS_N = '1' or rx_data_vld = '1')) then
+                    shreg_busy <= '1';
+                elsif (rx_data_vld = '1') then
+                    shreg_busy <= '0';
                 else
-                    last_bit_en <= '0';
+                    shreg_busy <= shreg_busy;
                 end if;
             end if;
         end if;
     end process;
+
+    -- The SPI slave is ready for accept new input data when CS_N is assert and
+    -- shift register not busy or when received data are valid.
+    slave_ready <= (CS_N and not shreg_busy) or rx_data_vld;
+    
+    -- The new input data is loaded into the shift register when the SPI slave
+    -- is ready and input data are valid.
+    load_data_en <= slave_ready and DIN_VLD;
+
+    -- -------------------------------------------------------------------------
+    --  DATA SHIFT REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The shift register holds data for sending to master, capture and store
+    -- incoming data from master.
+    data_shreg_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (load_data_en = '1') then
+                data_shreg <= DIN;
+            elsif (spi_clk_redge_en = '1' and CS_N = '0') then
+                data_shreg <= data_shreg(6 downto 0) & MOSI;
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  MISO REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The output MISO register ensures that the bits are transmit to the master
+    -- when is not assert CS_N and falling edge of SPI clock is detected.
+    miso_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (load_data_en = '1') then
+                MISO <= DIN(7);
+            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
+                MISO <= data_shreg(7);
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  ASSIGNING OUTPUT SIGNALS
+    -- -------------------------------------------------------------------------
+    
+    READY    <= slave_ready;
+    DOUT     <= data_shreg;
+    DOUT_VLD <= rx_data_vld;
 
 end RTL;
